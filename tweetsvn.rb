243bd996b393b72862e899e1b61348efd89bae09
Louis Rose <louismrose@gmail.com>
2010-08-11 09:31:51 +0100

    Allowed secrets to be initialised from environment variables.

--- tweetsvn.rb
+++ tweetsvn.rb
@@ -10,23 +10,33 @@ require 'xauth_consumer'
 
 class Tweetsvn
   def run
-    secrets = load_secrets_from_yaml
+    load_secrets
     
-    source  = GmailCommitSource.new secrets['source']['username'],
-                                    secrets['source']['password']
+    source  = GmailCommitSource.new @secrets['source']['username'],
+                                    @secrets['source']['password']
     
-    consumer = XauthConsumer.new secrets['twitter']['consumer']['key'],
-                                 secrets['twitter']['consumer']['secret']
+    consumer = XauthConsumer.new @secrets['twitter']['consumer']['key'],
+                                 @secrets['twitter']['consumer']['secret']
 
     tweeter = Tweeter.new consumer,
-                          secrets['twitter']['account']['username'],
-                          secrets['twitter']['account']['password']
+                          @secrets['twitter']['account']['username'],
+                          @secrets['twitter']['account']['password']
     
     UpdateTwitterFromSourceTransaction.new(source,tweeter).run
   end
 
 private
-  def load_secrets_from_yaml
-    YAML::load_file('secrets.yml')
+  def load_secrets
+    if File.exists?('secrets.yml')
+      @secrets = YAML::load_file('secrets.yml')
+    else
+      @secrets['source']['username'] = ENV['TWEETSVN_SOURCE_USERNAME']
+      @secrets['source']['password'] = ENV['TWEETSVN_SOURCE_PASSWORD']
+      
+      @secrets['twitter']['consumer']['key']    = ENV['TWEETSVN_TWITTER_CONSUMER_KEY']
+      @secrets['twitter']['consumer']['secret'] = ENV['TWEETSVN_TWITTER_CONSUMER_SECRET']
+      @secrets['twitter']['account']['username'] = ENV['TWEETSVN_TWITTER_CONSUMER_KEY']
+      @secrets['twitter']['account']['password'] = ENV['TWEETSVN_TWITTER_CONSUMER_KEY']
+    end
   end
 end
